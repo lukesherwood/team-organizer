43c96245b415618427dc69fe4a9271e44a522054
lukesherwood <luke.sherwood55@gmail.com>
2020-06-24 07:31:45 +1200

    updated notes and refactored js

--- NOTES.md
+++ NOTES.md
@@ -1,6 +1,10 @@
 #NOTES
 
 ##Do Next
-user rails setup
+user rails setup - do I need any gems bcrypt? or devise?
+
+create JS Class for Events?
+rest of event CRUD
 
 
+CSS and styling
--- team-organizer-frontend/src/index.js
+++ team-organizer-frontend/src/index.js
@@ -1,24 +1,24 @@
-const base_url = "http://localhost:3000";
+const baseUrl = 'http://localhost:3000'
 
-fetch(`${base_url}/events`)
+fetch(`${baseUrl}/events`)
   .then((response) => response.json())
-  .then((object) => object.forEach((obj) => createEvent(obj)));
+  .then((object) => object.forEach((obj) => createEvent(obj)))
 
 // fetch(`${base_url}/events/1`)
 //   .then((response) => response.json())
 //   .then((object) => renderEvent(object));
 
-function createEvent(data) {
-  //create JS event class and then pass to render function
-  renderEvent(data);
+function createEvent (data) {
+  // create JS event class and then pass to render function
+  renderEvent(data)
 }
 
-function renderEvent(obj) {
-  //this should take a JS class instance and create html to be displayed on DOM
-  const list = document.getElementById("event-list");
-  const listItem = document.createElement("li");
-  listItem.className = "event-list-item";
-  listItem.textContent = obj.name;
+function renderEvent (obj) {
+  // this should take a JS class instance and create html to be displayed on DOM
+  const list = document.getElementById('event-list')
+  const listItem = document.createElement('li')
+  listItem.className = 'event-list-item'
+  listItem.textContent = obj.name
 
-  list.appendChild(listItem);
+  list.appendChild(listItem)
 }
