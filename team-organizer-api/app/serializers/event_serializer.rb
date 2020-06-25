da5a075d972052cf2b65fd4664691dd5d2471fc9
lukesherwood <luke.sherwood55@gmail.com>
2020-06-25 12:41:00 +1200

    added relationships to serializer

--- team-organizer-api/app/serializers/event_serializer.rb
+++ team-organizer-api/app/serializers/event_serializer.rb
@@ -1,3 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :start_time, :end_time, :description, :location, :creator_id
+  has_many :users
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
 end
--- team-organizer-api/app/serializers/user_serializer.rb
+++ team-organizer-api/app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone_number, :team_id
+  has_many :events
+  belongs_to :team
+  belongs_to :event
 end
