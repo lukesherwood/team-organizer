a08f7f14c15182d58865fd8c92bf8e28be6eb503
lukesherwood <luke.sherwood55@gmail.com>
2020-06-29 13:32:33 +1200

    JS adding event info once clicked

--- team-organizer-frontend/src/components/event.js
+++ team-organizer-frontend/src/components/event.js
@@ -1,30 +1,40 @@
 class Event {
   constructor (eventJSON, teamName) {
+    this.id = eventJSON.id
     this.name = eventJSON.name
     this.startTime = eventJSON.start_time
     this.endTime = eventJSON.end_time
     this.description = eventJSON.description
     this.location = eventJSON.location
     this.teamName = teamName
-    this.renderEvent()
+    this.renderEventLink()
     this.eventListenerAndBindings()
   }
 
-  renderEvent () {
+  renderEventLink () {
     const team = document.getElementById(`${this.teamName}-events-list`)
-    const event = document.createElement('li')
-    event.className = 'event-list-item'
-    event.innerHTML = `<a href='#' id='${this.name}-event-link'>${this.name}</a>`
-    team.appendChild(event)
+    const eventLink = document.createElement('li')
+    eventLink.className = 'event-list-item'
+    eventLink.innerHTML = `<a href='#' id='event${this.id}-link'>${this.name}</a>`
+    team.appendChild(eventLink)
   }
 
   eventListenerAndBindings () {
-    this.eventLinks = document.getElementById(`${this.name}-event-link`)
-    this.eventLinks.addEventListener('click', this.showEvent())
+    this.eventLinks = document.getElementById(`event${this.id}-link`)
+    this.eventLinks.addEventListener('click', this.renderEvent.bind(this))
     // console.log(this.eventLinks[0].innerHTML)
   }
 
-  showEvent () {
-    console.log('fired') // is firing before I click??
+  renderEvent () {
+    // need to display event
+    const eventLink = document.getElementById(`event${this.id}-link`)
+    const eventInfoContainer = document.getElementById(`event${this.id}-info-container`) || document.createElement('div')
+    eventInfoContainer.id = `event${this.id}-info-container` // this is a little wasteful, changing ID even if already set
+    eventInfoContainer.innerHTML = this.eventInfoHtml()
+    eventLink.appendChild(eventInfoContainer)
+  }
+
+  eventInfoHtml () {
+    return `<p>${this.startTime} - ${this.endTime}<br>${this.description}</p>` // etc
   }
 }
