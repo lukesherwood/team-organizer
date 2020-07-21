5559e502fc237d2e2cac40bcf2a169a0fdbd6f9e
lukesherwood <luke.sherwood55@gmail.com>
2020-07-22 07:15:46 +1200

    refactored event class

--- team-organizer-frontend/src/components/event.js
+++ team-organizer-frontend/src/components/event.js
@@ -1,4 +1,4 @@
-class Event {
+class Event { // this class renders events and it's players
   constructor (eventJSON) {
     this.id = eventJSON.id
     this.name = eventJSON.name
@@ -10,12 +10,31 @@ class Event {
     this.rawPlayers = eventJSON.players
     this.players = []
     this.adapter = new EventAdapter()
-    this.renderEventLink()
-    this.renderButtons()
+    this.renderEventItems()
     this.eventListenerAndBindings()
   }
 
-  renderButtons () {
+  // Event render functions
+
+  renderEvent () {
+    this.eventLinkDiv = document.getElementById(`event${this.id}-div`)
+    this.eventInfoContainer = document.getElementById(`event${this.id}-info-container`) || document.createElement('div')
+    this.eventInfoContainer.id = `event${this.id}-info-container`
+    this.eventInfoContainer.innerHTML = this.eventInfoHtml()
+    this.eventLinkDiv.appendChild(this.eventInfoContainer)
+    this.renderPlayersHtml()
+    this.createPlayers()
+  }
+
+  eventInfoHtml () {
+    return `<p>${this.convertDateTime(this.startTime)} - ${this.convertDateTime(this.endTime)} </p> 
+    <p>${this.location}</p>
+    <p>${this.description}</p>
+    `
+  }
+
+  renderEventItems () {
+    this.renderEventLink()
     this.renderDeleteButton()
     this.renderEventInfoButton()
     this.renderUpdateButton()
@@ -58,101 +77,6 @@ class Event {
     this.buttonContainer.appendChild(this.eventUpdateButton)
   }
 
-  eventListenerAndBindings () {
-    this.eventLinks = document.getElementById(`event${this.id}-link`)
-    this.eventLinks.addEventListener('click', () => {
-      if (this.eventInfoContainer) {
-        this.eventInfoContainer.innerHTML = ''
-        this.eventInfoContainer = false
-        this.eventInfoButton.innerText = 'More Info'
-      } else {
-        this.renderEvent()
-        this.eventInfoButton.innerText = 'Less Info'
-      }
-    })
-    const deleteButton = document.getElementById(`event-${this.id}delete-event-button`)
-    deleteButton.addEventListener('click', () => {
-      if (confirm('Are you sure you want to remove this event?')) {
-        this.adapter.destroyEvent(this.team.id, this.id).then(() => {
-          const eventInfo = document.getElementById(`event${this.id}-div`)
-          eventInfo.parentNode.remove()
-        })
-      }
-    })
-    const updateButton = document.getElementById(`event-${this.id}update-event-button`)
-    updateButton.addEventListener('click', () => {
-      if (this.eventInfoContainer) {
-        this.eventLinks.click() // closes more info if open
-      }
-      this.renderUpdateForm()
-    })
-  }
-
-  renderUpdateForm () {
-    const event = document.getElementById(`event${this.id}-div`)
-    this.updateForm = document.getElementById(`event${this.id}-update-form-container`) || document.createElement('div')
-    this.updateForm.className = 'card'
-    this.updateForm.id = `event${this.id}-update-form-container`
-    this.updateForm.innerHTML = this.updateFormHtml()
-    event.appendChild(this.updateForm)
-    this.updateEventListeners()
-  }
-
-  renderEvent () {
-    this.eventLinkDiv = document.getElementById(`event${this.id}-div`)
-    this.eventInfoContainer = document.getElementById(`event${this.id}-info-container`) || document.createElement('div')
-    this.eventInfoContainer.id = `event${this.id}-info-container`
-    this.eventInfoContainer.innerHTML = this.eventInfoHtml()
-    this.eventLinkDiv.appendChild(this.eventInfoContainer)
-    this.renderPlayersHtml()
-    this.createPlayers()
-  }
-
-  updateEventListeners () {
-    const closeUpdateFormButton = document.getElementById(`close-event${this.id}-create-form`)
-    closeUpdateFormButton.addEventListener('click', () => {
-      this.updateForm.innerHTML = ''
-    })
-    this.updateEventForm = document.getElementById(`update-event${this.id}-form`)
-    this.updateEventForm.addEventListener('submit', (event) => {
-      event.preventDefault()
-      this.processUpdateEventForm()
-    })
-  }
-
-  processUpdateEventForm () {
-    const eventName = document.getElementById('eventName')
-    const eventDesc = document.getElementById('eventDesc')
-    const location = document.getElementById('location')
-    const startTime = document.getElementById('startTime')
-    const endTime = document.getElementById('endTime')
-    this.adapter.updateEvent(eventName.value, eventDesc.value, location.value, startTime.value, endTime.value, this.team.id, this.id).then((event) => {
-      const oldEventCard = document.getElementById(`event${this.id}-div`)
-      oldEventCard.parentNode.remove() // delete old event card
-      const updatedEvent = new Event(event) // create new event for dom
-      this.eventLinks.click() // open more info
-    })
-  }
-
-  renderPlayersHtml () {
-    this.eventInfoContainer = document.getElementById(`event${this.id}-info-container`)
-    const playersInfoContainer = document.createElement('div')
-    playersInfoContainer.className = 'card'
-    playersInfoContainer.id = `event${this.id}-players-container`
-    playersInfoContainer.innerHTML = `
-      <h5 class="card-header text-white" style="background-color: rgb(38, 101, 122); opacity: 80%; padding: 2px; text-align: center;">Players</h5>
-      <ul class="list-group list-group-flush" id="event-${this.id}-players-list-container"></ul>
-      <button class='btn btn btn-outline-primary btn-sm add-player-button' id="event-${this.id}add-player-button">Sign up for this event</button>`
-    this.eventInfoContainer.appendChild(playersInfoContainer)
-    new Players(this)
-  }
-
-  createPlayers () {
-    this.rawPlayers.forEach(player => {
-      this.players.push(new Player(player, this.id))
-    })
-  }
-
   convertDateTime (date) {
     function pad (n) {
       if (n === 3) { // ternary here?
@@ -177,11 +101,85 @@ class Event {
     return `${convertHours(hours)}:${pad(minutes)} ${pad(day)}/${pad(month)}/${year}`
   }
 
-  eventInfoHtml () {
-    return `<p>${this.convertDateTime(this.startTime)} - ${this.convertDateTime(this.endTime)} </p> 
-    <p>${this.location}</p>
-    <p>${this.description}</p>
-    `
+  // event listeners
+  eventListenerAndBindings () {
+    this.moreInfoListener()
+    this.deleteButtonListener()
+    this.updateButtonListener()
+  }
+
+  moreInfoListener () {
+    this.eventLinks = document.getElementById(`event${this.id}-link`)
+    this.eventLinks.addEventListener('click', () => {
+      if (this.eventInfoContainer) {
+        this.eventInfoContainer.innerHTML = ''
+        this.eventInfoContainer = false
+        this.eventInfoButton.innerText = 'More Info'
+      } else {
+        this.renderEvent()
+        this.eventInfoButton.innerText = 'Less Info'
+      }
+    })
+  }
+
+  deleteButtonListener () {
+    const deleteButton = document.getElementById(`event-${this.id}delete-event-button`)
+    deleteButton.addEventListener('click', () => {
+      if (confirm('Are you sure you want to remove this event?')) {
+        this.adapter.destroyEvent(this.team.id, this.id).then(() => {
+          const eventInfo = document.getElementById(`event${this.id}-div`)
+          eventInfo.parentNode.remove()
+        })
+      }
+    })
+  }
+
+  updateButtonListener () {
+    const updateButton = document.getElementById(`event-${this.id}update-event-button`)
+    updateButton.addEventListener('click', () => {
+      if (this.eventInfoContainer) {
+        this.eventLinks.click() // closes more info if open
+      }
+      this.renderUpdateForm()
+    })
+  }
+
+  updateEventListeners () {
+    const closeUpdateFormButton = document.getElementById(`close-event${this.id}-create-form`)
+    closeUpdateFormButton.addEventListener('click', () => {
+      this.updateForm.innerHTML = ''
+    })
+    this.updateEventForm = document.getElementById(`update-event${this.id}-form`)
+    this.updateEventForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+      this.processUpdateEventForm()
+    })
+  }
+
+  // Update form functions
+
+  renderUpdateForm () {
+    const event = document.getElementById(`event${this.id}-div`)
+    this.updateForm = document.getElementById(`event${this.id}-update-form-container`) || document.createElement('div')
+    this.updateForm.className = 'card'
+    this.updateForm.id = `event${this.id}-update-form-container`
+    this.updateForm.innerHTML = this.updateFormHtml()
+    event.appendChild(this.updateForm)
+    this.updateEventListeners()
+  }
+
+  processUpdateEventForm () {
+    const eventName = document.getElementById('eventName')
+    const eventDesc = document.getElementById('eventDesc')
+    const location = document.getElementById('location')
+    const startTime = document.getElementById('startTime')
+    const endTime = document.getElementById('endTime')
+    this.adapter.updateEvent(eventName.value, eventDesc.value, location.value, startTime.value, endTime.value, this.team.id, this.id).then((event) => {
+      const oldEventCard = document.getElementById(`event${this.id}-div`)
+      oldEventCard.parentNode.remove() // delete old event card
+      const updatedEvent = new Event(event) // create new event for dom
+      this.eventLinks.click() // open more info
+    })
   }
 
   updateFormHtml () {
@@ -213,5 +211,26 @@ class Event {
     </form>
     </div>
     `
+  }
+
+  // Event players functions
+
+  renderPlayersHtml () {
+    this.eventInfoContainer = document.getElementById(`event${this.id}-info-container`)
+    const playersInfoContainer = document.createElement('div')
+    playersInfoContainer.className = 'card'
+    playersInfoContainer.id = `event${this.id}-players-container`
+    playersInfoContainer.innerHTML = `
+      <h5 class="card-header text-white" style="background-color: rgb(38, 101, 122); opacity: 80%; padding: 2px; text-align: center;">Players</h5>
+      <ul class="list-group list-group-flush" id="event-${this.id}-players-list-container"></ul>
+      <button class='btn btn btn-outline-primary btn-sm add-player-button' id="event-${this.id}add-player-button">Sign up for this event</button>`
+    this.eventInfoContainer.appendChild(playersInfoContainer)
+    new Players(this)
+  }
+
+  createPlayers () {
+    this.rawPlayers.forEach(player => {
+      this.players.push(new Player(player, this.id))
+    })
   }
 }
